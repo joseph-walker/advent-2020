3681ea8d26f101d05dd1ad8f3d2bc361d910289e
Joseph Walker <jwalker@rentpath.com>
2020-12-06 20:13:39 -0600

    Day 6 pt 1

---
+++ day6/main.hs
@@ -0,0 +1,8 @@
+import Data.List
+import Data.List.Split ( splitOn )
+
+main :: IO ()
+main = do
+    input <- splitOn "\n\n" <$> readFile "input.txt"
+    print "Part 1"
+    print . sum $ fmap (length . fmap head . group . sort . concat . lines) input
