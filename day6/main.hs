281ba903bbb96e7947c158915a7a1872ba67ab7c
Joseph Walker <jwalker@rentpath.com>
2020-12-06 23:53:35 -0600

    Day 6 pt 2

--- day6/main.hs
+++ day6/main.hs
@@ -1,8 +1,19 @@
 import Data.List
 import Data.List.Split ( splitOn )
 
+solve :: [String] -> Int
+solve input =
+    let 
+        numInputs = length input
+        countedInputs = 
+            fmap length . group . sort . concat $ input
+    in
+        length $ filter (== numInputs) countedInputs
+
 main :: IO ()
 main = do
     input <- splitOn "\n\n" <$> readFile "input.txt"
     print "Part 1"
     print . sum $ length . fmap head . group . sort . concat . lines <$> input
+    print "Part 2"
+    print . sum $ solve . lines <$> input
