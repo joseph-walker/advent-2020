efcdedac9abf2432c71d302bbb244984028f10ea
Joseph Walker <jwalker@rentpath.com>
2020-12-06 20:16:04 -0600

    Formatting

--- day6/main.hs
+++ day6/main.hs
@@ -5,4 +5,4 @@ main :: IO ()
 main = do
     input <- splitOn "\n\n" <$> readFile "input.txt"
     print "Part 1"
-    print . sum $ fmap (length . fmap head . group . sort . concat . lines) input
+    print . sum $ length . fmap head . group . sort . concat . lines <$> input
